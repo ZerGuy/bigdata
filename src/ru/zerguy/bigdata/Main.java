a4308646e15684a1bd5b698b602c0211220e86f9
Sergei Olenev <sergei_olenev@epam.com>
2016-12-05 19:16:10 +0300

    http get

--- src/ru/zerguy/bigdata/Main.java
+++ src/ru/zerguy/bigdata/Main.java
@@ -9,25 +9,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import static org.apache.http.HttpHeaders.USER_AGENT;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
-        String url = "http://www.reddit.com/api/v1/me";
+    public static final String KEY = "18A4FFCA6F72D1EB21FE386E521D1695";
+    public static final String STEAM_ID = "76561198023043869";
+
+    public static void main(String[] args) throws IOException {
+        String url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=18A4FFCA6F72D1EB21FE386E521D1695&steamids=76561198023043869";
 
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(url);
 
-        // add request header
-        request.addHeader("User-Agent", USER_AGENT);
         HttpResponse response = client.execute(request);
 
-        System.out.println("Response Code : "
-                + response.getStatusLine().getStatusCode());
+        System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
 
-        BufferedReader rd = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent()));
+        BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
         StringBuffer result = new StringBuffer();
         String line = "";
