68fdebb2da427ef25a99605c3dc5ef777f77004c
Sergei Olenev <sergei_olenev@epam.com>
2016-12-05 14:27:47 +0300

    initial

---
+++ src/ru/zerguy/bigdata/Main.java
@@ -0,0 +1,41 @@
+package ru.zerguy.bigdata;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static org.apache.http.HttpHeaders.USER_AGENT;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String url = "http://www.reddit.com/api/v1/me";
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+
+        // add request header
+        request.addHeader("User-Agent", USER_AGENT);
+        HttpResponse response = client.execute(request);
+
+        System.out.println("Response Code : "
+                + response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
+        System.out.println(result);
+
+    }
+}
